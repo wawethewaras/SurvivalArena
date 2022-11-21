1931739122ebcd8782c4a747e34c287bc0bbe19c
Mika Savolainen <111045320+ElementalEcho@users.noreply.github.com>
2022-11-21 18:53:02 +0200

    Add project files.

---
+++ SurvivalArena/Game1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGamePractice {
+    internal class Game1 : Game {
+
+        private GraphicsDeviceManager graphicsDeviceManager;
+
+        private SpriteBatch spriteBatch = null;
+
+        private SpriteFont? font = null;
+        public Game1() : base() {
+            graphicsDeviceManager = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsFixedTimeStep = true;
+            this.IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            base.Initialize();
+        }
+        protected override void LoadContent() {
+            this.spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("FontTest");
+
+        }
+        protected override void UnloadContent() {
+            base.UnloadContent();
+        }
+        protected override void Update(GameTime gameTime) {
+            base.Update(gameTime);
+        }
+        protected override void Draw(GameTime gameTime) {
+            this.GraphicsDevice.Clear(Color.CornflowerBlue);
+            this.spriteBatch.Begin();
+            this.spriteBatch.DrawString(font,"Hello!", new Vector2(0,0), Color.White);
+            this.spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SurvivalArena/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MonoGamePractice {
+
+    public class Program {
+        public static void Main(string[] args) {
+            using (Game1 game = new Game1()) {
+                game.Run();
+            }
+        }
+    }
+}
